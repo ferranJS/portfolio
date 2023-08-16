4a46173648f5f04a9d0f6e844dd355fb8b76752f
Ferran <ferranramirezmarti@gmail.com>
2023-08-16 19:14:11 +0200

    automatic language translation to English, Español, 한국어, Français, Deutsch, Italiano, Русский

---
+++ src/i18n/languages.ts
@@ -0,0 +1,157 @@
+export const languages = {
+  en: "English",
+  es: "Español",
+  ko: "한국어",
+  fr: "Français",
+  de: "Deutsch",
+  it: "Italiano",
+  ru: "Русский",
+}
+
+export const defaultLang = "en"
+
+export const uiTranslations = {
+  en: {
+    phrase1: "Committed to delivering top-notch solutions by adhering to industry best practices.",
+    phrase2:
+      "Actively dedicated to expanding my skill set by staying at the forefront of technological advancements.",
+    phrase3:
+      "From very light and fast landing pages to complex, secure and reactive native mobile applications.",
+    metaDescription:
+      "Ferran Ramírez Martí is a software engineer, who graduated in Computer Science in UPV (Spain), with 2 years of experience as a Full Stack developer. Experience in Angular, Astro, React, RxJS Firebase, Ionic, Capacitor, SQL, and CSS with Tailwind ✅ Applying the best practices and up-to-date with other technologies that make up Full Stack development, how they correlate with each other and their importance and power in the industry 🔎 FerranJS",
+
+    potatostudios: "Digital art studio.",
+    tasle: "Responsive and lighter Tesla page clone with automatic language detection and region selection.",
+    lavelada3remake:
+      "Revamped and interactive fighting match website featuring immersive animations upon hover. A modernized take on the original concept, enhancing user engagement and interactivity.",
+    discordish:
+      "A Discord (messaging web app) clone focused on user experience and performance by creating a good architectural design and a well thought and designed database structure.",
+  },
+  es: {
+    Home: "Inicio",
+    Projects: "Proyectos",
+    Contact: "Contacto",
+    phrase1:
+      "Comprometido a entregar soluciones de primera categoría al adherirse a las mejores prácticas de la industria.",
+    phrase2:
+      "Dedicado activamente a expandir mi conjunto de habilidades al mantenerme a la vanguardia de los avances tecnológicos.",
+    phrase3:
+      "Desde páginas de aterrizaje muy ligeras y rápidas hasta aplicaciones móviles nativas complejas, seguras y reactivas.",
+    and: "y",
+    metaDescription:
+      "Ferran Ramírez Martí es un desarrollador de software, graduado en Ingeniería informatica Informática en la UPV (España), con 2 años de experiencia como desarrollador Full Stack. Experiencia en Angular, Astro, React, RxJS Firebase, Ionic, Capacitor, SQL y CSS con Tailwind ✅ Aplicando las mejores prácticas y al día con otras tecnologías que componen el desarrollo Full Stack, cómo se correlacionan entre ellas y su importancia y potencia en la industria 🔎 FerranJS",
+
+    potatostudios: "Estudio de arte digital.",
+    tasle:
+      "Clon de la página de Tesla, más ligera y responsive, con detección automática de idioma y selección de región.",
+    lavelada3remake:
+      "Sitio web de combates rediseñado e interactivo con animaciones inmersivas al pasar el ratón. Una versión modernizada del concepto original, mejorando la participación e interactividad del usuario.",
+    discordish:
+      "Un clon de Discord (aplicación web de mensajería) centrado en la experiencia del usuario y el rendimiento mediante la creación de un buen diseño arquitectónico y una estructura de base de datos bien pensada y diseñada.",
+  },
+  ko: {
+    Home: "홈",
+    Projects: "프로젝트",
+    Contact: "연락처",
+    phrase1: "산업의 모범 사례를 준수하여 최고 수준의 솔루션을 제공하는 것을 약속합니다.",
+    phrase2: "기술적 발전의 최전선에 머무르며 기술 스택을 확장하는 데 헌신합니다.",
+    phrase3:
+      "매우 가볍고 빠른 랜딩 페이지부터 복잡하고 안전하며 반응성이 뛰어난 네이티브 모바일 애플리케이션까지.",
+    and: "그리고",
+    metaDescription:
+      "Ferran Ramírez Martí는 스페인 UPV에서 컴퓨터 공학을 전공한 소프트웨어 엔지니어로, 풀 스택 개발자로 2년의 경력을 보유하고 있습니다. 앵귤러, 아스트로, 리액트, RxJS 파이어베이스, 아이오닉, 커패시터, SQL 및 CSS와 테일윈드 경험 ✅ 풀 스택 개발을 구성하는 다른 기술에 대한 모범 사례 적용 및 최신 정보, 서로의 상관관계, 업계에서의 중요성 및 영향력 🔎 FerranJS",
+
+    potatostudios: "디지털 아트 스튜디오.",
+    tasle: "자동 언어 감지 및 지역 선택이 포함된 반응형 및 가벼운 테슬라 페이지 복제본.",
+    lavelada3remake:
+      "마우스 오버시 몰입형 애니메이션을 특징으로하는 개편 및 대화형 전투 웹 사이트. 원래 개념을 현대화하여 사용자 참여도와 상호 작용성을 향상시킵니다.",
+    discordish:
+      "사용자 경험과 성능에 중점을 둔 Discord (메시징 웹 앱) 클론. 잘 설계된 아키텍처 및 데이터베이스 구조를 만들어 사용자 경험과 성능을 향상시킵니다.",
+  },
+  ru: {
+    Home: "Главная",
+    Projects: "Проекты",
+    Contact: "Контакты",
+    phrase1: "Обязуюсь предоставлять решения высшего класса, придерживаясь лучших практик отрасли.",
+    phrase2:
+      "Активно посвящен расширению своего набора навыков, оставаясь на переднем крае технологических достижений.",
+    phrase3:
+      "От очень легких и быстрых целевых страниц до сложных, безопасных и реактивных нативных мобильных приложений.",
+    and: "и",
+    metaDescription:
+      "Ferran Ramírez Martí - инженер-программист, окончил факультет компьютерных наук в UPV (Испания), имеет двухлетний опыт работы в качестве Full Stack разработчика. Опыт работы с Angular, Astro, React, RxJS Firebase, Ionic, Capacitor, SQL и CSS с Tailwind ✅ Применение лучших практик и актуальное знакомство с другими технологиями, составляющими Full Stack разработку, их взаимосвязь между собой, важность и сила в индустрии 🔎 FerranJS",
+
+    potatostudios: "Студия цифрового искусства.",
+    tasle:
+      "Отзывчивый и более легкий клон страницы Tesla с автоматическим определением языка и выбором региона.",
+    lavelada3remake:
+      "Переработанный и интерактивный сайт боя с захватывающими анимациями при наведении. Современная версия оригинальной концепции, улучшающая вовлеченность и взаимодействие пользователя.",
+    discordish:
+      "Клон Discord (веб-приложение для обмена сообщениями), сосредоточенный на пользовательском опыте и производительности, создающий хорошую архитектурную конструкцию и продуманную и спроектированную структуру базы данных.",
+  },
+  de: {
+    Home: "Startseite",
+    Projects: "Projekte",
+    Contact: "Kontakt",
+    phrase1:
+      "Verpflichtet, erstklassige Lösungen zu liefern, indem die Best Practices der Branche eingehalten werden.",
+    phrase2:
+      "Aktiv bestrebt, mein Fähigkeiten zu erweitern, indem ich an vorderster Front der technologischen Fortschritte bleibe.",
+    phrase3:
+      "Von sehr leichten und schnellen Landing Pages bis hin zu komplexen, sicheren und reaktiven nativen mobilen Anwendungen.",
+    and: "und",
+    metaDescription:
+      "Ferran Ramírez Martí ist ein Software-Ingenieur mit einem Abschluss in Informatik an der UPV (Spanien) und 2 Jahren Erfahrung als Full-Stack-Entwickler. Erfahrung in Angular, Astro, React, RxJS Firebase, Ionic, Capacitor, SQL und CSS mit Tailwind ✅ Anwendung der besten Praktiken und aktuell mit anderen Technologien, die die Full Stack Entwicklung ausmachen, wie sie miteinander korrelieren und ihre Bedeutung und Macht in der Industrie 🔎 FerranJS",
+
+    potatostudios: "Digitales Kunststudio.",
+    tasle:
+      "Reaktionsfähiger und leichterer Tesla-Seiten-Klon mit automatischer Spracherkennung und Regionsauswahl.",
+    lavelada3remake:
+      "Überarbeitete und interaktive Kampfspiel-Website mit immersiven Animationen beim Überfahren. Eine modernisierte Version des ursprünglichen Konzepts, die die Benutzerbindung und -interaktivität verbessert.",
+    discordish:
+      "Ein Discord (Messaging-Web-App) Klon, der sich auf Benutzererfahrung und Leistung konzentriert, indem er ein gutes architektonisches Design und eine gut durchdachte und gestaltete Datenbankstruktur erstellt.",
+  },
+  it: {
+    Home: "Inizio",
+    Projects: "Progetti",
+    Contact: "Contatto",
+    phrase1: "Impegnato a fornire soluzioni di prim'ordine aderendo alle migliori pratiche del settore.",
+    phrase2:
+      "Attivamente impegnato ad ampliare il mio set di abilità rimanendo all'avanguardia dei progressi tecnologici.",
+    phrase3:
+      "Da landing page molto leggere e veloci ad applicazioni mobili native complesse, sicure e reattive.",
+    and: "e",
+    metaDescription:
+      "Ferran Ramírez Martí è un ingegnere informatico, laureato in Informatica a UPV (Spagna), con 2 anni di esperienza come sviluppatore Full Stack. Esperienza in Angular, Astro, React, RxJS Firebase, Ionic, Capacitor, SQL e CSS con Tailwind ✅ Applicazione delle migliori pratiche e aggiornamento con le altre tecnologie che compongono lo sviluppo Full Stack, come si correlano tra loro e la loro importanza e potenza nel settore 🔎 FerranJS",
+
+    potatostudios: "Studio di arte digitale.",
+    tasle:
+      "Clone della pagina Tesla più leggero e reattivo con rilevamento automatico della lingua e selezione della regione.",
+    lavelada3remake:
+      "Sito web di combattimento rivisitato e interattivo con animazioni coinvolgenti al passaggio del mouse. Una versione modernizzata del concetto originale, migliorando l'interazione e l'interattività dell'utente.",
+    discordish:
+      "Un clone di Discord (applicazione web di messaggistica) incentrato sull'esperienza utente e sulle prestazioni creando un buon design architettonico e una struttura di database ben pensata e progettata.",
+  },
+  fr: {
+    Home: "Accueil",
+    Projects: "Projets",
+    Contact: "Contact",
+    phrase1:
+      "Engagé à fournir des solutions de premier ordre en adhérant aux meilleures pratiques de l'industrie.",
+    phrase2:
+      "Activement dédié à élargir mon ensemble de compétences en restant à la pointe des progrès technologiques.",
+    phrase3:
+      "Des pages d'atterrissage très légères et rapides aux applications mobiles natives complexes, sécurisées et réactives.",
+    and: "et",
+    metaDescription:
+      "Ferran Ramírez Martí est un ingénieur logiciel, diplômé en informatique à UPV (Espagne), avec 2 ans d'expérience en tant que développeur Full Stack. Expérience en Angular, Astro, React, RxJS Firebase, Ionic, Capacitor, SQL et CSS avec Tailwind ✅ Appliquer les meilleures pratiques et à jour avec d'autres technologies qui composent le développement Full Stack, comment ils sont en corrélation les uns avec les autres et leur importance et leur puissance dans l'industrie 🔎 FerranJS.",
+
+    potatostudios: "Studio d'art numérique.",
+    tasle:
+      "Clone de page Tesla plus léger et réactif avec détection automatique de la langue et sélection de la région.",
+    lavelada3remake:
+      "Site Web de combat repensé et interactif avec des animations immersives au passage de la souris. Une version modernisée du concept original, améliorant l'engagement et l'interactivité des utilisateurs.",
+    discordish:
+      "Un clone de Discord (application Web de messagerie) axé sur l'expérience utilisateur et les performances en créant une bonne conception architecturale et une structure de base de données bien pensée et conçue.",
+  },
+} as const
